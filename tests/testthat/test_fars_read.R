c8a924c0b1723b90e02649c86970842184afecfa
Russell <russell.politzky>
2017-04-30 13:35:35 +0200

    Build should be working now. Added system.file("extdata", file_name, package = "fars") to access files properly.

--- tests/testthat/test_fars_read.R
+++ tests/testthat/test_fars_read.R
@@ -1,6 +1,9 @@
+
+
+
 test_that('FARS file can be loaded', {
   file_name <- fars::make_filename(2013)
-  full_path <- file.path("../../inst/extdata/", file_name)
+  full_path <- system.file("extdata", file_name, package = "fars")
   data      <- fars::fars_read(full_path)
   no_rows   <- nrow(data)
   expect_equal(no_rows, 30202)
@@ -9,7 +12,7 @@ test_that('FARS file can be loaded', {
 
 test_that('FARS has correct number of columns', {
   file_name <- fars::make_filename(2013)
-  full_path <- file.path("../../inst/extdata/", file_name)
+  full_path <- system.file("extdata", file_name, package = "fars")
   data      <- fars::fars_read(full_path)
   no_cols   <- length(data)
   expect_equal(no_cols, 50)
